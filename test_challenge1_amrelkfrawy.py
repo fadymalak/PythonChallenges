563e6652be3f8028151b7e369b744b8b2b641282
Amr Elkfrawy <amrelkfrawy@gmail.com>
2024-07-17 13:43:47 +0300

    test: add unit test for Book

--- test_challenge1_amrelkfrawy.py
+++ test_challenge1_amrelkfrawy.py
@@ -25,5 +25,10 @@ class TestLibrary(unittest.TestCase):
         self.assertEqual(self.lib.find_book_by_title("Fluent Python"), self.book)
         self.assertEqual(self.lib.find_book_by_title("Fluent Python1"), 'Book not found')
 
+class TestBook(unittest.TestCase):
+    def test_str(self):
+        book = Book("Fluent Python", "Luciano Ramalho", 2015)
+        self.assertEqual(str(book), "Fluent Python - Luciano Ramalho 2015")
+
 if __name__ == "__main__":
     unittest.main()
